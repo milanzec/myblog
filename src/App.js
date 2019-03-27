761866ab4526b8b0c2396276aee8e5cf03c76939
BIT Student <classroom@bgit.rs>
2019-03-27 18:35:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import post from './post';
+
+const Header = () => {
+  return (
+    < header>
+      <h1>My React Blog</h1>
+    </header>
+  )
+}
+const PrintPost = () => {
+  return (
+    post.map((element) => {
+      return (
+        <div className="siva">
+          <h2>
+            {element.title}
+          </h2>
+          <p>
+            {element.body}
+          </p>
+        </div>
+      )
+    })
+  )
+
+
+}
+
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header /> ,
+      <PrintPost />
+
     );
   }
 }
